bed73bdeb1bdd5d1af26048dcf3bbb98ca38325a
TheSola10 <me@thesola.io>
2023-02-19 15:34:36 +0100

    Adding universal binary to flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,22 @@
     (system:
     let pkgs = import nixpkgs { inherit system; };
     in {
-      packages.fakedir = pkgs.callPackage ./default.nix {};
+      packages = rec {
+        default = fakedir;
+        fakedir = pkgs.callPackage ./default.nix {};
+        fakedir-universal = pkgs.stdenv.mkDerivation {
+          inherit (fakedir) version;
+          pname = "fakedir-universal";
+
+          nativeBuildInputs = with pkgs;
+          [ bintools-unwrapped ];
+          installPhase = ''
+            mkdir -p $out
+            cp ${self.packages.x86_64-darwin.fakedir}/lib/libfakedir.dylib libfakedir.x86_64.dylib
+            cp ${self.packages.aarch64-darwin.fakedir}/lib/libfakedir.dylib libfakedir.aarch64.dylib
+            lipo -create -arch x86_64 libfakedir.x86_64.dylib -arch arm64 libfakedir.aarch64.dylib -output $out/libfakedir.dylib
+          '';
+        };
+      };
     });
 }
